31ca990288be3fcd31d7d7faaa7ea4a842e8dd77
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-08 18:12:45 -0300

    Update ConversaoRomana.java
    generalizando com  um HashMap

--- src/tdd/estudo1/ConversaoRomana.java
+++ src/tdd/estudo1/ConversaoRomana.java
@@ -1,9 +1,23 @@
 package tdd.estudo1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConversaoRomana {
+	
+	private static Map<String, Integer> tabela =
+			new HashMap<String, Integer>() {{
+			put("I", 1);
+			put("V", 5);
+			put("X", 10);
+			put("L", 50);
+			put("C", 100);
+			put("D", 500);
+			put("M", 1000);
+	}};
+	
 	public int converte(String numero) {
-		if (numero.equals("I")) return 1;
-		else if(numero.equals("V")) return 5;
-		return 0;
+		return tabela.get(numero);
+
 	}
 }
