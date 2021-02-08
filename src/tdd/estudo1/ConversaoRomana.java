2bc26f33b4a3048878d0e0557b5e02690741dcd9
JeanCarlos2017 <jean__100limites@hotmail.com>
2021-02-08 18:02:40 -0300

    Iniciando com TDD

---
+++ src/tdd/estudo1/ConversaoRomana.java
@@ -0,0 +1,7 @@
+package tdd.estudo1;
+
+public class ConversaoRomana {
+	public int converte(String numero) {
+		return 1;
+	}
+}
---
+++ test/tdd/estudo1Test/ConversaoRomanaTest.java
@@ -0,0 +1,14 @@
+package tdd.estudo1Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConversaoRomanaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
